80ec3e0ea93e46a9e3c8f197a372d5c308f2b401
ginKkool <ginkko71@gmail.com>
2025-06-11 17:10:14 +0200

    project completed

--- react-counter/src/App.jsx
+++ react-counter/src/App.jsx
@@ -1,28 +1,53 @@
 import "./App.css";
 import { useState } from "react";
+import { FaCalculator } from "react-icons/fa";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
 
   return (
     <>
-      <p>{counter}</p>
-      {counter} = 0 ? :
-      <button
-        onClick={() => {
-          setCounter(counter - 1);
-        }}
-      >
-        -
-      </button>
-      : <button></button>
-      <button
-        onClick={() => {
-          setCounter(counter + 1);
-        }}
-      >
-        +
-      </button>
+      <header>
+        <FaCalculator size={60} />
+
+        <h1>React Counter</h1>
+      </header>
+      <main>
+        <div className="container">
+          <div>
+            {counter !== 0 ? (
+              <button
+                onClick={() => {
+                  setCounter(counter - 1);
+                }}
+              >
+                -
+              </button>
+            ) : null}
+          </div>
+          <p>{counter}</p>
+          <div>
+            {counter !== 10 ? (
+              <button
+                onClick={() => {
+                  setCounter(counter + 1);
+                }}
+              >
+                +
+              </button>
+            ) : null}
+          </div>
+        </div>
+        <button
+          className="reset"
+          type="button"
+          onClick={() => {
+            setCounter(0);
+          }}
+        >
+          Reset
+        </button>
+      </main>
     </>
   );
 };
